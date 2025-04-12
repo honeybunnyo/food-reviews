48e3ef4f96c1d3296b7b46f8e258cf191fb45b68
Jacqueline Ta <z5361055@ad.unsw.edu.au>
2025-04-13 01:44:22 +1000

    Upload error UI added

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -7,6 +7,8 @@ import PasswordField from './../components/InputField/PasswordField';
 import RatingField from './../components/InputField/RatingField';
 import TextAreaField from './../components/InputField/TextAreaField';
 import ImageField from './../components/InputField/ImageField';
+import ErrorToast from './../components/Toast/ErrorToast';
+import SuccessToast from './../components/Toast/SuccessToast';
 
 const page = () => {
   const [formData, setFormData] = useState({
@@ -32,6 +34,8 @@ const page = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [imageFile, setImageFile] = useState(null);
+  const [successToast, setSuccessToast] = useState(false);
+  const [failToast, setFailToast] = useState(false);
 
   const handleChange = (field) => (value) => {
     setFormData(prev => ({ ...prev, [field]: value }));
@@ -55,9 +59,11 @@ const page = () => {
       method: 'POST',
       body: formToSend,
     });
-    const data = await res.json();
-    console.log(data);
-
+    if (res.ok) {
+      setSuccessToast(true)
+    } else {
+      setFailToast(true)
+    }
   }
 
   const validateField = () => {
@@ -72,7 +78,6 @@ const page = () => {
   
     setErrors(newErrors)
     const hasErrors = Object.values(newErrors).some(Boolean)
-    console.log('hasErrors', newErrors)
     if (hasErrors) return false
     return true
   }
@@ -82,33 +87,43 @@ const page = () => {
   };
 
   return (
-    <form className="m-10" autoComplete="off" onSubmit={handleSubmit} noValidate>
-      <ContentDropdown content={formData.content} setContent={handleChange('content')} hasError={errors.content}/>
-      <InputField label="title" content={formData.title} setContent={handleChange('title')} hasError={errors.title}/>
-      <TextAreaField label="description" content={formData.description} setContent={handleChange('description')}/>
+    <div>
+      <form className="m-10" autoComplete="off" onSubmit={handleSubmit} noValidate>
+        <ContentDropdown content={formData.content} setContent={handleChange('content')} hasError={errors.content}/>
+        <InputField label="title" content={formData.title} setContent={handleChange('title')} hasError={errors.title}/>
+        <TextAreaField label="description" content={formData.description} setContent={handleChange('description')}/>
 
-      {formData.content === 'recipes' && (
-        <>
-          <TextAreaField label="recipe" content={formData.recipe} setContent={handleChange('recipe')} />
-          <TextAreaField label="method" content={formData.method} setContent={handleChange('method')} />
-        </>
-      )}
+        {formData.content === 'recipes' && (
+          <>
+            <TextAreaField label="recipe" content={formData.recipe} setContent={handleChange('recipe')} />
+            <TextAreaField label="method" content={formData.method} setContent={handleChange('method')} />
+          </>
+        )}
 
-      {formData.content === 'restaurants' && (
-        <>
-          <TextAreaField label="entree" content={formData.entree} setContent={handleChange('entree')} />
-          <TextAreaField label="main" content={formData.main} setContent={handleChange('main')} />
-          <TextAreaField label="dessert" content={formData.dessert} setContent={handleChange('dessert')} />
-        </>
-      )}
-      <RatingField rating={formData.rating} setRating={handleChange('rating')} hasError={errors.rating}/>
-      <label className="block text-sm font-medium mb-2">background image</label>
-      <ImageField imageFile={imageFile} handleImageSelect={handleImageSelect} hasError={errors.file}/>
-      <PasswordField setUsername={setUsername} setPassword={setPassword} hasUsernameError={errors.username} hasPasswordError={errors.password}/>
-      <button type="submit" className="text-white bg-gray-700 hover:bg-gray-800 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
-        Upload
-      </button>
-    </form>
+        {formData.content === 'restaurants' && (
+          <>
+            <TextAreaField label="entree" content={formData.entree} setContent={handleChange('entree')} />
+            <TextAreaField label="main" content={formData.main} setContent={handleChange('main')} />
+            <TextAreaField label="dessert" content={formData.dessert} setContent={handleChange('dessert')} />
+          </>
+        )}
+        <RatingField rating={formData.rating} setRating={handleChange('rating')} hasError={errors.rating}/>
+        <label className="block text-sm font-medium mb-2">background image</label>
+        <ImageField imageFile={imageFile} handleImageSelect={handleImageSelect} hasError={errors.file}/>
+        <PasswordField setUsername={setUsername} setPassword={setPassword} hasUsernameError={errors.username} hasPasswordError={errors.password}/>
+        <button type="submit" className="text-white bg-gray-700 hover:bg-gray-800 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
+          Upload
+        </button>
+      </form>
+      <div className="fixed bottom-6 left-1/2 transform -translate-x-1/2 z-50">
+      {successToast && (
+          <SuccessToast setVisible={setSuccessToast}/>
+        )}
+        {failToast && (
+          <ErrorToast setVisible={setFailToast}/>
+        )}
+        </div>
+    </div>
   );
 }
 
