d572133fa82dfe239978f614d60bce932223af22
Jacqueline Ta <z5361055@ad.unsw.edu.au>
2025-04-16 09:08:25 +1000

    Replace image upload field with drag and drop

--- src/app/components/InputField/ImageField.jsx
+++ src/app/components/InputField/ImageField.jsx
@@ -1,15 +1,7 @@
 import React from 'react'
-
-import { useRef } from 'react'
+import Dropzone from './Dropzone';
 
 const ImageField = ({ imageFiles = [], setImagesByCategory, hasError = false, multiple = false, category }) => {
-  const fileInputRef = useRef(null);
-
-  const handleFileChange = (e) => {
-    const files = Array.from(e.target.files || []);
-    handleImageSelect(files);
-  };
-
   const handleImageRemove = (category, indexToRemove) => {
     setImagesByCategory(prev => ({
       ...prev,
@@ -27,17 +19,7 @@ const ImageField = ({ imageFiles = [], setImagesByCategory, hasError = false, mu
   return (
     <div className="gap-6 mb-6 w-1/2 min-w-[20rem] max-w-[30rem]">
       {category && <label className="block text-sm font-medium mb-2">{category}</label>}
-      <input
-        ref={fileInputRef}
-        type="file"
-        accept="image/*"
-        multiple={multiple}
-        onChange={handleFileChange}
-        className={`block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 ${
-          hasError ? 'border-red-500 bg-red-100' : ''
-        }`}
-      />
-
+      <Dropzone category={category} handleImageSelect={handleImageSelect} multiple={multiple} />
       {imageFiles.length > 0 && (
         <div className="flex gap-2 mt-4 flex-wrap">
           {imageFiles.map((file, idx) => (
