7d191c3be21e197e7555643edaf482bb7402078e
Jacqueline Ta <z5361055@ad.unsw.edu.au>
2025-04-21 04:02:13 +1000

    Update import

--- src/app/api/test-db/route.js
+++ src/app/api/test-db/route.js
@@ -1,5 +1,5 @@
 // /app/api/test-db/route.ts
-import { prisma } from '@/lib/prisma'
+import { prisma } from '../../lib/prisma'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
